1c8863a96f540fa3e47feaf0b36cbc7aad939439
Richard Sternagel <richard.sternagel@1und1.de>
2016-01-12 22:17:14 +0100

    add vue-resource

--- src/js/app.js
+++ src/js/app.js
@@ -1,11 +1,14 @@
 require('css!../css/main.css');
 
 var Vue = require('vue')
+var VueResource = require('vue-resource')
 
 var Map = require('./map.vue')
 var City = require('./city.vue')
 
 Vue.config.debug = true
+
+Vue.use(VueResource)
 
 new Vue({
   el: 'body',
